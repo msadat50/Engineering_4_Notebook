0431bb2d26fe7e665db82012f6f7a67a7423fcf8
msadat50 <71345399+msadat50@users.noreply.github.com>
2022-09-15 13:26:28 -0400

    Update LaunchPadPart1(Countdown).py

--- raspberry-pi/LaunchPadPart1(Countdown).py
+++ raspberry-pi/LaunchPadPart1(Countdown).py
@@ -8,6 +8,7 @@ ledblue=digitalio.DigitalInOut(board.GP15)
 ledblue.direction=digitalio.Direction.OUTPUT
 
 for a in range(10,0,-1):
+        if a is not 0:
         ledblue.value=True 
         print (a)
         time.sleep(0.5)
